e0d83c086dab42bac157697ed6e822b5421a8d26
nereidrbx <nereidscripts@gmail.com>
2023-06-11 12:49:56 +0300

    Initial commit

---
+++ newcodes.lua
@@ -0,0 +1,12 @@
+{
+    "ThanksFor350MVisits2",
+    "ThanksFor350MVisits",
+    "ThanksForLikes600K",
+    "ThanksForLikes600K2",
+    "Upd295MiniCode",
+    "NewAntiExploit",
+    "Thx4300MNOuwohanaIsBack",
+    "SorryForIssuesToday",
+    "Miniupdate1.5",
+    "Update1.5Spins"
+  }
