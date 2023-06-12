adecc1c95cb667bf7bc4fe41016d5bdc2decb936
nereidrbx <nereidscripts@gmail.com>
2023-06-12 11:22:57 +0300

    Update newcodes.lua

--- newcodes.lua
+++ newcodes.lua
@@ -3,10 +3,5 @@ getgenv()._G.ClanCodesTable = {
   "ThanksFor350MVisits",
   "ThanksForLikes600K",
   "ThanksForLikes600K2",
-  "Upd295MiniCode",
-  "NewAntiExploit",
-  "Thx4300MNOuwohanaIsBack",
-  "SorryForIssuesToday",
-  "Miniupdate1.5",
-  "Update1.5Spins"
+  "Thx4300MNOuwohanaIsBack"
 }
