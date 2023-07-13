05182e2b8c11ff236b8bd75d8e0145ed9f027ee8
nereidrbx <136233472+nereidrbx@users.noreply.github.com>
2023-07-13 23:49:08 +0300

    Update newcodes.lua

--- newcodes.lua
+++ newcodes.lua
@@ -1,7 +1,3 @@
 getgenv()._G.ClanCodesTable = {
-  "ThanksFor350MVisits2",
-  "ThanksFor350MVisits",
-  "ThanksForLikes600K",
-  "ThanksForLikes600K2",
-  "Thx4300MNOuwohanaIsBack"
+  "1millfavs"
 }
